c4d7ec44415a504d6c24e818b0a0286997fa3b82
Jose Varanam <jvaranam@gmail.com>
2017-08-02 15:30:21 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
+import { MenuPage } from '../pages/menu/menu';
+import { TabsPage } from '../pages/tabs/tabs';
+import { Tab1Page } from '../pages/tab1/tab1';
+import { Tab2Page } from '../pages/tab2/tab2';
+import { SpecialPage } from '../pages/special/special';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage,
+    MenuPage,
+    TabsPage,
+    Tab1Page,
+    Tab2Page,
+    SpecialPage,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    MenuPage,
+    TabsPage,
+    Tab1Page,
+    Tab2Page,
+    SpecialPage,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
